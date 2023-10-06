6f988844037f76e94621e13ba312b12e9e913f12
furiax <carlmalfliet@proximus.be>
2023-10-06 21:13:22 +0200

    created a add ride button and created the create page

---
+++ MVC.Logger.Furiax/Logger/Pages/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Logger.Pages
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
