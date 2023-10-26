e47788e1fa0b263ffb4f34f8481b257eb42072cb
wkktoria <127852624+wkktoria@users.noreply.github.com>
2023-10-27 01:27:56 +0200

    Add search box

--- DrinksLogger.wkktoria/DrinksLogger.wkktoria/wwwroot/js/site.js
+++ DrinksLogger.wkktoria/DrinksLogger.wkktoria/wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function search() {
+    let input = document.getElementById("searchbar");
+    let keyword = input.value.toUpperCase();
+    let table = document.getElementById("drinks");
+    let rows = table.getElementsByTagName("tr");
 
-// Write your JavaScript code.
+    for (let i = 1; i < rows.length; i++) {
+        let columns = rows[i].getElementsByTagName("td");
+        
+        for (let j = 0; j < columns.length - 2; j++) {
+            if (columns[j]) {
+                let textValue = columns[j].innerText.trim().toUpperCase();
+                console.log(textValue)
+
+                if (textValue.indexOf(keyword) > -1) {
+                    rows[i].style.display = "";
+                    break;
+                } else {
+                    rows[i].style.display = "none";
+                }
+            }
+        }
+    }
+}
