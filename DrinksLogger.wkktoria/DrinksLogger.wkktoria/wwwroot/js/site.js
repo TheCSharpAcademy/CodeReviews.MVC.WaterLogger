7a87619f4fd68f8a8473d722a882451a88fde1d8
wkktoria <127852624+wkktoria@users.noreply.github.com>
2023-10-27 01:48:03 +0200

    Refactor style

--- DrinksLogger.wkktoria/DrinksLogger.wkktoria/wwwroot/js/site.js
+++ DrinksLogger.wkktoria/DrinksLogger.wkktoria/wwwroot/js/site.js
@@ -1,12 +1,12 @@
 ﻿function search() {
-    let input = document.getElementById("searchbar");
+    let input = document.getElementById("searchbox");
     let keyword = input.value.toUpperCase();
     let table = document.getElementById("drinks");
     let rows = table.getElementsByTagName("tr");
 
     for (let i = 1; i < rows.length; i++) {
         let columns = rows[i].getElementsByTagName("td");
-        
+
         for (let j = 0; j < columns.length - 2; j++) {
             if (columns[j]) {
                 let textValue = columns[j].innerText.trim().toUpperCase();
