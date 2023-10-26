c56254a67035573bb91deecbbd918e6d29f90f77
wkktoria <127852624+wkktoria@users.noreply.github.com>
2023-10-27 00:30:52 +0200

    Add option to add drink

--- DrinksLogger.wkktoria/DrinksLogger.wkktoria/Pages/Index.cshtml.cs
+++ DrinksLogger.wkktoria/DrinksLogger.wkktoria/Pages/Index.cshtml.cs
@@ -1,18 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Globalization;
+using DrinksLogger.wkktoria.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
 
 namespace DrinksLogger.wkktoria.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _configuration;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(IConfiguration configuration)
     {
-        _logger = logger;
+        _configuration = configuration;
     }
 
-    public void OnGet()
+    public List<Drink> Drinks { get; set; } = null!;
+
+    public IActionResult OnGet()
     {
+        Drinks = GetAllDrinks();
+
+        return Page();
+    }
+
+    private List<Drink> GetAllDrinks()
+    {
+        using var connection = new SqliteConnection(_configuration.GetConnectionString("DefaultConnection"));
+        var drinks = new List<Drink>();
+
+        try
+        {
+            connection.Open();
+
+            var command = connection.CreateCommand();
+
+            command.CommandText =
+                "SELECT * FROM drinks";
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+                drinks.Add(
+                    new Drink
+                    {
+                        Id = reader.GetInt32(0),
+                        Date = DateTime.Parse(reader.GetString(1),
+                            CultureInfo.CurrentUICulture.DateTimeFormat),
+                        Type = reader.GetString(2),
+                        Measurement = reader.GetString(3),
+                        Quantity = reader.GetDouble(4)
+                    });
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            connection.Close();
+        }
+
+        return drinks;
     }
 }
