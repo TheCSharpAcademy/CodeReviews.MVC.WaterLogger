b8fa1aa6b2f402f0ab0697c18ab2679247165288
simon davies <daviessimon21@gmail.com>
2023-12-25 00:10:26 +0000

    Added view all records, add record and delete record.

--- WaterLogger.Speedierone/WaterLogger.Speedierone/Pages/Index.cshtml.cs
+++ WaterLogger.Speedierone/WaterLogger.Speedierone/Pages/Index.cshtml.cs
@@ -1,20 +1,50 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
+using System.Globalization;
+using WaterLogger.Speedierone.Model;
 
 namespace WaterLogger.Speedierone.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+       
+        public List<MovieLoggerModel> Movies { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
+        public IndexModel(IConfiguration configuration)
+        {    
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
+            Movies = GetAllRecords();
+        }
 
+        private List<MovieLoggerModel> GetAllRecords()
+        {
+            using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText =
+                    $@"SELECT * FROM MovieLogger";
+                var tableData = new List<MovieLoggerModel>();
+                SqliteDataReader reader = tableCmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    tableData.Add(new MovieLoggerModel()
+                    {
+                        Id = reader.GetInt32(0),
+                        MovieName = reader.GetString(1),
+                        MovieGenre = reader.GetString(2),
+                        MovieWatchDate = DateTime.Parse(reader.GetString(3), CultureInfo.CurrentUICulture.DateTimeFormat)
+                    });
+                }
+                return tableData;
+            }
         }
     }
 }
