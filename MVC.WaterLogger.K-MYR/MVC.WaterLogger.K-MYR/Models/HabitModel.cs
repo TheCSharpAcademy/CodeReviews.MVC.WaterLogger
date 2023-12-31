d5272b53771e1fed968e14b7f89f5adc869a4a94
K-MYR <144546255+K-MYR@users.noreply.github.com>
2023-12-31 14:58:18 +0000

    Added Validation

--- MVC.WaterLogger.K-MYR/MVC.WaterLogger.K-MYR/Models/HabitModel.cs
+++ MVC.WaterLogger.K-MYR/MVC.WaterLogger.K-MYR/Models/HabitModel.cs
@@ -1,11 +1,18 @@
-﻿namespace MVC.WaterLogger.K_MYR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.WaterLogger.K_MYR.Models
 {
     public class HabitModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Measurement { get; set; }
-        public string Icon { get; set; }
+        [Required]
+        [StringLength(250, MinimumLength =1, ErrorMessage = "Please enter a valid name (0-250 characters)!")]               
+        public string? Name { get; set; }
+        [Required]
+        [StringLength(250, MinimumLength =1, ErrorMessage = "Please enter a valid name (0-250 characters)!")]  
+        public string? Measurement { get; set; }
+        [Required]        
+        public string? Icon { get; set; }
         public ICollection<RecordModel> Records { get; set; } = new List<RecordModel>();
     }
 }
--- MVC.WaterLogger.K-MYR/MVC.WaterLogger.K-MYR/Models/RecordModel.cs
+++ MVC.WaterLogger.K-MYR/MVC.WaterLogger.K-MYR/Models/RecordModel.cs
@@ -1,10 +1,14 @@
-﻿namespace MVC.WaterLogger.K_MYR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.WaterLogger.K_MYR.Models
 {
     public class RecordModel
     {
-        public int Id { get; set; }
+        public int Id { get; set; }              
         public DateTime Date { get; set; }
-        public float Quantity { get; set; }
+        [Required]  
+        [Range(0, float.MaxValue, ErrorMessage = "Invalid Input!")]
+        public float Quantity { get; set; }        
         public int HabitId { get; set; }
     }
 }
