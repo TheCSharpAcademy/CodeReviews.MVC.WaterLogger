4da3735d667dcb9d2eb5f0bc77209a5c148f6aff
Vocal <vocalnight@protonmail.com>
2024-01-01 16:52:32 -0300

    project done

---
+++ WaterLogger.VocalNight/Pages/Index.cshtml.cs
@@ -0,0 +1,53 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
+using WaterDrinkingLogger.Models;
+
+namespace WaterDrinkingLogger.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly IConfiguration _configuration;
+
+    public List<DrinkingWaterModel> Records { get; set; }
+
+    public IndexModel( IConfiguration configuration )
+    {
+        _configuration = configuration;
+    }
+
+    public void OnGet()
+    {
+        Records = GetAllRecords();
+        ViewData["Total"] = Records.AsEnumerable().Sum(x => x.Quantity);
+    }
+
+    private List<DrinkingWaterModel> GetAllRecords()
+    {
+        using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+        {
+            connection.Open();
+            var tableCmd = connection.CreateCommand();
+            tableCmd.CommandText =
+                $"SELECT * FROM drinking_water ";
+
+            var tableData = new List<DrinkingWaterModel>();
+            SqliteDataReader reader = tableCmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                tableData.Add(
+                new DrinkingWaterModel
+                {
+                    Id = reader.GetInt32(0),
+                    Date = DateTime.Parse(reader.GetString(1), 
+                        CultureInfo.CurrentUICulture.DateTimeFormat),
+                    Quantity = reader.GetDouble(2)
+                }); ;
+            }
+
+            return tableData;
+        }
+
+    }
+}
---
+++ WaterLogger.VocalNight/README.md
@@ -0,0 +1,3 @@
+### WaterDrinkingLogger
+
+Small project to get familiar with razor web pages in dotnet.
