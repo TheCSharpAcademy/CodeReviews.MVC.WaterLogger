860100851bf9c09e825f3bc9da5f5c64353bfe7d
K-MYR <144546255+K-MYR@users.noreply.github.com>
2024-01-03 11:06:03 +0100

    Minor Improvements And Bug Fixes

--- MVC.WaterLogger.K-MYR/MVC.WaterLogger.K-MYR/Models/HabitModel.cs
+++ MVC.WaterLogger.K-MYR/MVC.WaterLogger.K-MYR/Models/HabitModel.cs
@@ -7,15 +7,15 @@ namespace MVC.WaterLogger.K_MYR.Models
         public int Id { get; set; }
         [Required]
         [StringLength(250, MinimumLength = 1, ErrorMessage = "Please enter a valid name (1-250 characters)!")]
-        [RegularExpression(@"\S+", ErrorMessage = "The field cannot consist of only whitespaces!")]
+        [RegularExpression(@".*\S.*", ErrorMessage = "The field cannot consist of only whitespaces!")]
         public string? Name { get; set; }
         [Required]
         [StringLength(250, MinimumLength = 1, ErrorMessage = "Please enter a valid measurement unit (1-250 characters)!")]
-        [RegularExpression(@"\S+", ErrorMessage = "The field cannot consist of only whitespaces!")]
+        [RegularExpression(@".*\S.*", ErrorMessage = "The field cannot consist of only whitespaces!")]
         public string? Measurement { get; set; }
         [Required]
         [StringLength(250, MinimumLength = 1, ErrorMessage = "Please enter a valid measurement unit (1-250 characters)!")]
-        [RegularExpression(@"\S+", ErrorMessage = "The field cannot consist of only whitespaces!")]
+        [RegularExpression(@".*\S.*", ErrorMessage = "The field cannot consist of only whitespaces!")]
         public string? Icon { get; set; }
         public ICollection<RecordModel> Records { get; set; } = new List<RecordModel>();
     }
