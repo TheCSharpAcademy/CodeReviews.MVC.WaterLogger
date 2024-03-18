21c85a51d54a63f5d79a2df3715a24cd5a2aa9ed
ManuelE-Osorio <aux.manuel.osorio@gmail.com>
2024-03-18 17:57:06 -0500

    finished for review

--- WaterLogger.ManuelE-Osorio/README.md
+++ WaterLogger.ManuelE-Osorio/README.md
@@ -5,3 +5,8 @@
 ## References
 
 1. <https://learn.microsoft.com/en-us/aspnet/core/mvc/views/razor?view=aspnetcore-6.0>
+2. <https://learn.microsoft.com/en-us/aspnet/core/client-side/bundling-and-minification?view=aspnetcore-8.0>
+3. <https://learn.microsoft.com/en-us/aspnet/core/mvc/controllers/areas?view=aspnetcore-8.0>
+4. <https://learn.microsoft.com/en-us/aspnet/core/client-side/libman/libman-vs?view=aspnetcore-8.0>
+5. <https://getbootstrap.com/docs/5.3/getting-started/introduction/>
+6. <https://learn.microsoft.com/en-us/aspnet/core/mvc/views/layout?view=aspnetcore-6.0>
--- WaterLogger.ManuelE-Osorio/WaterLogger/wwwroot/js/site.js
+++ WaterLogger.ManuelE-Osorio/WaterLogger/wwwroot/js/site.js
@@ -2,20 +2,50 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-/*!
- * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
- * Copyright 2011-2024 The Bootstrap Authors
- * Licensed under the Creative Commons Attribution 3.0 Unported License.
- */
+
+window.onload = function() {
+    if(getCookie("isDark").includes("true")){
+        themeSwitch()
+    }
+}
 
 document.getElementById('btnSwitch').addEventListener('click',()=>{
+    themeSwitch()
+})
+
+function themeSwitch()
+{
     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
         document.documentElement.setAttribute('data-bs-theme','light') 
+        setCookie("isDark", "false", 2)
         document.querySelector("use[id=toggle-icon]").setAttribute('href','#sun-fill')
     }
     else {
         document.documentElement.setAttribute('data-bs-theme','dark')
+        setCookie("isDark", "true", 2)
         document.querySelector("use[id=toggle-icon]").setAttribute('href','#moon-stars-fill')
     }
-})
-  
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
