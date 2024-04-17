8d282a70f8024eef0bb6cd650a811a9983db6eaa
frockett <crockett.d.ford@gmail.com>
2024-04-17 15:08:14 +0800

    added data annotation to remove time from date display

--- Logger.frockett/Logger.frockett/Models/DailyPushupsModel.cs
+++ Logger.frockett/Logger.frockett/Models/DailyPushupsModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Logger.frockett.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Logger.frockett.Models;
 
 public class DailyPushupsModel
 {
 	public int Id { get; set; }
-	public DateTime Date { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    public DateTime Date { get; set; }
 	public int Quantity { get; set; }
 }
