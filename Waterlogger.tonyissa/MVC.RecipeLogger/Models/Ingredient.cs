9491745d982cbc484d6dd4a3930fa4d6492fa8ed
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-10-29 17:13:13 -0400

    Add validation to fields

--- Waterlogger.tonyissa/MVC.RecipeLogger/Models/Ingredient.cs
+++ Waterlogger.tonyissa/MVC.RecipeLogger/Models/Ingredient.cs
@@ -7,11 +7,15 @@ public class Ingredient
 {
     public int Id { get; set; }
     [Display(Name = "Ingredient name")]
+    [StringLength(50, MinimumLength = 1)]
     public string Name { get; set; } = string.Empty;
+    [Range(0.01, 100.00)]
     public float Amount { get; set; }
     [Display(Name = "Measure (example: tbsp, cups)")]
+    [StringLength(25, MinimumLength = 1)]
     public string Measure { get; set; } = string.Empty;
     [Display(Name = "Comment (optional)")]
+    [StringLength(200)]
     public string? Comment { get; set; } = string.Empty;
     public int RecipeId { get; set; }
     public Recipe Recipe { get; set; }
--- Waterlogger.tonyissa/MVC.RecipeLogger/Models/Recipe.cs
+++ Waterlogger.tonyissa/MVC.RecipeLogger/Models/Recipe.cs
@@ -7,7 +7,9 @@ public class Recipe
 {
     public int Id { get; set; }
     [Display(Name = "Recipe name")]
+    [StringLength(50, MinimumLength = 1)]
     public string Name { get; set; } = string.Empty;
     public List<Ingredient> Ingredients { get; set; } = [];
+    [StringLength(500, MinimumLength = 1)]
     public string Instructions { get; set; } = string.Empty;
 }
